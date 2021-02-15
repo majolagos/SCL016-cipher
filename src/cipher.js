ebc7671f24082555abf9cc0e28c0293190848ae3
majolagos <mlagos.r90@gmail.com>
2021-02-15 09:34:14 -0300

    actualizando proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,71 @@
-const cipher = {
-  // ...
-};
+
+let textoCifradoFinal = "";
+
+
+
+const cipher = { //creo mi objeto cipher
+
+
+  encode: (numero, texto)=> { 
+
+      
+    for(let i=0; i<texto.length; i++){ //metodo encode
+    
+      console.log(texto[i]);
+
+      let valorLetra = texto.charCodeAt(i);
+      console.log(valorLetra);
+
+      if(valorLetra >= 65 && valorLetra <=90){
+      
+        textoCifradoFinal = textoCifradoFinal + String.fromCharCode((valorLetra - 65 + parseInt(numero)) %26 + 65); 
+
+      }else if(valorLetra >= 97 && valorLetra <= 122){
+      
+        textoCifradoFinal = textoCifradoFinal + String.fromCharCode((valorLetra - 97 + parseInt(numero))%26+97);
+      
+      }else{
+      
+        textoCifradoFinal = textoCifradoFinal + texto[i];
+      }
+      
+      }return textoCifradoFinal;
+  
+
+
+
+  },
+ 
+  decode: (numero, texto) => { 
+
+    for(let i=0; i<texto.length; i++){ //metodo decode
+    
+      console.log(texto[i]);
+
+      let valorLetra = texto.charCodeAt(i);
+      console.log(valorLetra);
+
+      if(valorLetra >= 65 && valorLetra <=90){
+      
+        textoCifradoFinal = textoCifradoFinal + String.fromCharCode((valorLetra - 65 - parseInt(numero)) %26 + 65); 
+
+      }else if(valorLetra >= 97 && valorLetra <= 122){
+      
+        textoCifradoFinal = textoCifradoFinal + String.fromCharCode((valorLetra - 97 - parseInt(numero))%26+97);
+      
+      }else{
+      
+        textoCifradoFinal = textoCifradoFinal + texto[i];
+      }
+      
+      }return textoCifradoFinal;
+  
+  
+  }
+}
+
+
+
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let texto = "";
+let numero;
+let btnCerrarModal = document.getElementById("btnCerrarModal");
+let btnCifrar = document.getElementById("btnCifrar");
+let btnDecifrar = document.getElementById("btnDecifrar");
+let btnRegresar = document.getElementById("btnRegresar");
+
+
+
+
+    btnCerrarModal.addEventListener("click", ()=>{ //si hago clic en el boton btnCerrarModal ejecuto la funcion para ocultar el modal con CSS
+
+        document.getElementById("miModal").style.display = "none";
+
+    });
+
+
+
+    btnCifrar.addEventListener("click", () =>{ //si hago clic en el boton btnCifrar ejecuto funcion para cifrar texto
+        texto = document.getElementById("txtTexto").value;
+        numero = document.getElementById("slcCanal").value;
+
+        if (texto == null || texto == "" || numero == null || numero == ""){
+
+            alert("Oye! te faltó ingresar datos :c");
+        }else{
+
+
+            texto = texto.toUpperCase();        
+    
+            let cifrado =  cipher.encode(numero,texto);    
+            
+            document.getElementById("lblTextoCifrado").innerHTML = cifrado;
+        
+            document.getElementById("preguntas").style.display = "none";
+            document.getElementById("respuestas").style.display = "unset";
+        }
+    
+    });
+
+
+    btnDecifrar.addEventListener("click", () =>{ //si hago clic en el boton btnCifrar ejecuto funcion para cifrar texto
+        texto = document.getElementById("txtTexto").value;
+        numero = document.getElementById("slcCanal").value;
+
+        if (texto == null || texto == "" || numero == null || numero == ""){
+
+            alert("Oye! te faltó ingresar datos :c");
+        }else{
+
+
+            texto = texto.toUpperCase();        
+    
+            let decifrado =  cipher.decode(numero,texto);    
+            
+            document.getElementById("textoRespuesta").innerText = "Tu texto decifrado es: ";
+            document.getElementById("lblTextoCifrado").innerHTML = decifrado;        
+            document.getElementById("preguntas").style.display = "none";
+            document.getElementById("respuestas").style.display = "block";
+        }
+    
+    });
+
+    btnRegresar.addEventListener("click", () =>{ //VARIABLES A "" Y LIMPIAR HTML
+
+        document.getElementById("preguntas").style.display = "block";
+        document.getElementById("respuestas").style.display = "none";
+        // document.getElementById("lblTextoCifrado").innerHTML = "";
+        // document.getElementById("txtTexto").innerHTML = "";
+        // document.getElementById("slcCanal").innerText  ="";
+        // texto = "";
+        // numero = "";
+
+    })
+
+
+
